6abf133ffb631f6dc675a0a50c1f12f04823bd4b
garrettjj <garrett@jansen.sh>
2018-11-24 17:04:17 -0500

    Testing deploy

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site deploy@69.164.214.227:/data/nginx/blog
+#rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site deploy@69.164.214.227:/data/nginx/blog
+ssh deploy@69.164.214.227 echo "Great stuff!" > ~/sample.txt
