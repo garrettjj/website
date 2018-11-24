5255a06785c450cb0c5666343721382507c16e10
garrettjj <garrett@jansen.sh>
2018-11-24 17:49:51 -0500

    Further implementation troubleshooting.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 # Assorted troubleshooting of Travis errors
 ls -larth /home/travis/build/garrettjj/website/
-find /home/travis/build/ -type f "index.html" 
+find /home/travis/build/ -type f -name "index.html" 
 
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site deploy@69.164.214.227:/data/nginx/blog
 ssh deploy@69.164.214.227 echo "Most recent successful deployment: $TRAVIS_BUILD_ID"
