615a6a0de56f06e53cff89e867fbe73418be39be
garrettjj <garrett@jansen.sh>
2018-11-24 17:32:43 -0500

    Fixing implementation

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 
-ssh deploy@69.164.214.227 echo "Great stuff!" > ~/sample.txt
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site deploy@69.164.214.227:/data/nginx/blog
+ssh deploy@69.164.214.227 echo "Most recent successful deployment: $TRAVIS_BUILD_ID"
