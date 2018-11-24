7ecec4c76d3e72333b17deacf4568e27bb6cbf4a
garrettjj <garrett@jansen.sh>
2018-11-24 17:39:10 -0500

    Troubleshooting Travvy errors

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,4 +1,8 @@
 #/bin/bash
 
+# Assorted troubleshooting of Travis errors
+ls -larth /home/travis/build/garrettjj/website/
+find /home/travis/build/ -type f "index.html" 
+
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site deploy@69.164.214.227:/data/nginx/blog
 ssh deploy@69.164.214.227 echo "Most recent successful deployment: $TRAVIS_BUILD_ID"
