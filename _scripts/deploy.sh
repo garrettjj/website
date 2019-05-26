2c7736abcbb4ab24604736f049bf2da874aa4e38
garrettjj <garrett@jansen.sh>
2019-05-25 23:27:40 -0400

    Testing the removal of IP addresses

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,5 +4,5 @@
 ls -larth /home/travis/build/garrettjj/website/
 find /home/travis/build/ -type f -name "index.html" 
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site/ deploy@142.93.188.22:/data/blog/
-ssh deploy@142.93.188.22 echo "Most recent successful deployment: $TRAVIS_BUILD_ID"
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site/ deploy@internal.web.jansen.sh:/data/blog/
+ssh deploy@internal.web.jansen.sh echo "Most recent successful deployment: $TRAVIS_BUILD_ID"
