b1b5f9c677ed653b7bc9594bc4c8581139df9ac6
garrettjj <garrett@jansen.sh>
2019-05-25 23:12:27 -0400

    Updating to newer server

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,5 +4,5 @@
 ls -larth /home/travis/build/garrettjj/website/
 find /home/travis/build/ -type f -name "index.html" 
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site/ deploy@69.164.214.227:/data/nginx/blog/
-ssh deploy@69.164.214.227 echo "Most recent successful deployment: $TRAVIS_BUILD_ID"
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site/ deploy@142.93.188.22:/data/blog/
+ssh deploy@142.93.188.22 echo "Most recent successful deployment: $TRAVIS_BUILD_ID"
