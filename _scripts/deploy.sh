c56c06f2054143a4bff24a9a89a418ecd54cc033
jansendotsh <garrett@jansen.sh>
2020-08-22 11:17:44 -0700

    Updating deployment details

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Assorted troubleshooting of Travis errors
-ls -larth /home/travis/build/garrettjj/website/
+ls -larth /home/travis/build/jansendotsh/website/
 find /home/travis/build/ -type f -name "index.html" 
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site/ deploy@internal.web.jansen.sh:/data/blog/
-ssh deploy@internal.web.jansen.sh echo "Most recent successful deployment: $TRAVIS_BUILD_ID"
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site/ deploy@web001.infra.jansen.sh:/usr/share/nginx/blog/
+ssh deploy@web001.infra.jansen.sh echo "Most recent successful deployment: $TRAVIS_BUILD_ID"
