3ec5f08809e6010cd88ddbe1aeaab48df2fef31c
jansendotsh <garrett@jansen.sh>
2020-08-28 09:37:05 -0700

    updating naming

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,5 +4,5 @@
 ls -larth /home/travis/build/jansendotsh/website/
 find /home/travis/build/ -type f -name "index.html" 
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site/ deploy@web001.infra.jansen.sh:/usr/share/nginx/blog/
-ssh deploy@web001.infra.jansen.sh echo "Most recent successful deployment: $TRAVIS_BUILD_ID"
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site/ deploy@dream.jansen.sh:/usr/share/nginx/blog/
+ssh deploy@dream.jansen.sh echo "Most recent successful deployment: $TRAVIS_BUILD_ID"
